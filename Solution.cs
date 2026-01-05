6fc51a61eb04cee9bf0b91999b8bb0b0b88f1ae5
Vapos <Vaposs@bk.ru>
2026-01-05 21:18:36 +0300

    change2

--- Solution.cs
+++ Solution.cs
@@ -6,19 +6,34 @@ namespace ZD_7
     {
         public static void Main(string[] args)
         {
-
+            Weapon weapon = new Weapon(-1);
         }
     }
 
     class Weapon
     {
-        private const int MinBullets = 0;
-        private const int CostShotingBullets = 1;
+        private const int CostPerShot = 1;
 
+        private int _maxBullets;
         private int _bullets;
 
-        public bool CanShoot() => _bullets > MinBullets;
+        public Weapon(int maxBullets)
+        {
+            if (maxBullets < 0)
+                throw new ArgumentException("Максимальное количество пуль не может быть отрицательным", nameof(maxBullets));
 
-        public void Shoot() => _bullets -= CostShotingBullets;
+            _maxBullets = maxBullets;
+            _bullets = maxBullets;
+        }
+
+        public int Bullets => _bullets;
+        public int MaxBullets => _maxBullets;
+        public bool CanShoot => _bullets >= CostPerShot;
+
+        public void Shoot()
+        {
+            if (!CanShoot)
+            _bullets -= CostPerShot;
+        }
     }
 }
