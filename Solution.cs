e85539bfa7d5da7672b3abeeb19fff15a860c93c
Vapos <Vaposs@bk.ru>
2026-01-05 19:15:38 +0300

    addSolution

---
+++ Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ZD_7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Weapon
+    {
+        private const int MinBullets = 0;
+        private const int CostShotingBullets = 1;
+
+        private int _bullets;
+
+        public bool CanShoot() => _bullets > MinBullets;
+
+        public void Shoot() => _bullets -= CostShotingBullets;
+    }
+}
---
+++ StartScript.cs
@@ -0,0 +1,8 @@
+﻿class Weapon
+{
+    private int _bullets;
+
+    public bool CanShoot() => _bullets > 0;
+
+    public void Shoot() => _bullets -= 1;
+}
